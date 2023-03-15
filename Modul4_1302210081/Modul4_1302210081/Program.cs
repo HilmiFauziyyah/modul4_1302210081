69093793f0e07865474b5f0edb44e7c2cda6babe
hilmifauziyyah <hilmifs2113@gmail.com>
2023-03-15 16:22:30 +0700

    Menambahkan Class KodeBuah

---
+++ Modul4_1302210081/Modul4_1302210081/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Modul4_1302210081;
+
+kode_Buah kb = new kode_Buah();
+Console.WriteLine(kb.getKode_Buah(kode_Buah.buah.Jagung));
---
+++ Modul4_1302210081/Modul4_1302210081/kode Buah.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul4_1302210081
+{
+    internal class kode_Buah
+    {
+        public enum buah {Apel, Buah, Alpukat, Pisang, Paprika, Kurma, Durian, Anggur, Melon, Semangka, Blackberry, Ceri, Kelapa, Jagung}
+        String[] arr = { "A00", "B00", "C00", "D00", "E00", "K00", "L00", "M00", "N00", "O00", "F00", "H00", "I00", "J00" };
+        
+        public string getKode_Buah(buah namabuah) => arr[(int)namabuah];
+    }
+}
